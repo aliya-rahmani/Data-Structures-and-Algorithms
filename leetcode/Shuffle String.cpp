f64f7d820b012c5139a7777ec2940c8cdbb4946a
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-08-05 11:40:06 +0530

    #1528-EASY

---
+++ leetcode/Shuffle String.cpp
@@ -0,0 +1,14 @@
+// THE STRING NEEDS SOME VALUE TO ITERATE IT CAN'T BE EMPTY
+
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string temp=s;
+        for(int i=0;i<indices.size();i++){
+            temp[indices[i]] = s[i]  ;
+        }
+        
+        return temp;
+    }
+};
