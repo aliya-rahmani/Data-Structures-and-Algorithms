698d3972349db2bf7ac39faf56916c8f642795ba
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-09-01 23:28:14 +0530

    Update matrix.cpp

--- implementation/matrix.cpp
+++ implementation/matrix.cpp
@@ -12,3 +12,12 @@ int main()
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 2; j++)
+		{
+			cout << "Element at x[" << i
+				<< "][" << j << "]: ";
+			cout << x[i][j]<<endl;
+		}
+	}
+
+	return 0;
+}
