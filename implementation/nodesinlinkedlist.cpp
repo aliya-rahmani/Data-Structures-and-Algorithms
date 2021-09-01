40bf50b4d1b5083f52232f73c12e29eefc488492
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-09-01 23:25:51 +0530

    Create nodesinlinkedlist.cpp

---
+++ implementation/nodesinlinkedlist.cpp
@@ -0,0 +1,81 @@
+// CPP program to rearrange nodes
+// as alternate odd even nodes in
+// a Singly Linked List
+#include <bits/stdc++.h>
+using namespace std;
+
+// Structure node
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+// A utility function to print
+// linked list
+void printList(struct Node* node)
+{
+	while (node != NULL) {
+		cout << node->data << " ";
+		node = node->next;
+	}
+	cout << endl;
+}
+
+// Function to create newNode
+// in a linkedlist
+Node* newNode(int key)
+{
+	Node* temp = new Node;
+	temp->data = key;
+	temp->next = NULL;
+	return temp;
+}
+
+// Function to insert at beginning
+Node* insertBeg(Node* head, int val)
+{
+	Node* temp = newNode(val);
+	temp->next = head;
+	head = temp;
+	return head;
+}
+
+// Function to rearrange the
+// odd and even nodes
+void rearrangeOddEven(Node* head)
+{
+	stack<Node*> odd;
+	stack<Node*> even;
+	int i = 1;
+
+	while (head != nullptr) {
+
+		if (head->data % 2 != 0 && i % 2 == 0) {
+
+			// Odd Value in Even Position
+			// Add pointer to current node
+			// in odd stack
+			odd.push(head);
+		}
+
+		else if (head->data % 2 == 0 && i % 2 != 0) {
+
+			// Even Value in Odd Position
+			// Add pointer to current node
+			// in even stack
+			even.push(head);
+		}
+
+		head = head->next;
+		i++;
+	}
+
+	while (!odd.empty() && !even.empty()) {
+
+		// Swap Data at the top of two stacks
+		swap(odd.top()->data, even.top()->data);
+		odd.pop();
+		even.pop();
+	}
+}
+
