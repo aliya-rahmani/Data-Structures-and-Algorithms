5c79ab1fde68bf3de7ea084cc070ed331b46670f
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-09-09 15:59:06 +0530

    Update 3.Merge two sorted array.cpp

--- Striver's 180/3.Merge two sorted array.cpp
+++ Striver's 180/3.Merge two sorted array.cpp
@@ -2,36 +2,34 @@
 
 class Solution{
     public:
-    
-    int Nextgap(gap){
-      if(gap<=1)
-        return 0;
-      return (gap/2) + (gap%2);//ceiling for odds 
-    }
-
-
-    void merge(int* arr1, int* arr2, int n, int m){
-      int i,j,gap=m+n;
-      for(gap = Nextgap(gap);gap>0; gap=Nextgap(gap)){
-        //swapping in the first array and sorting it
-        for(i=0; i+gap<n; i+=gap){
-          if(arr1[i]>arr1[i+gap])
-            swap(arr1[i],arr1[i+gap]);
+        //Function to merge the arrays.
+        int nextgap(int gap){
+            if(gap<=1)
+             return 0;
+             
+            return (gap/2)+(gap%2); 
         }
-        //swapping among both arrays  
-        for(j=gap>n?gap-n:0; i < n && j < m; i++, j++){
-          if(arr1[i]>arr2[j]
-             swap(arr1[i],arr2[j]);
-        }
-        //swapping in the second array and sorting it
-        if(j<m){
-          for(j=0; j+gap<m; j+=gap){
-            if(arr2[j]>arr2[j+gap])
-               swap(arr2[j],arr2[j+gap]);
-        
-        }     
-                      
-      }
-}
-
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+           int i,j, gap = m+n;    
+           for(gap = nextgap(gap); gap>0 ; gap = nextgap(gap) ){
+               //first array swapping
+               for(i = 0;i+gap<n;i++){
+                   if(arr1[i]>arr1[i+gap]){
+                       swap(arr1[i],arr1[i+gap]);
+                   }
+               }
+               //inter-array swapping
+               for(j = gap > n ? gap - n : 0; i<n && j<m;i++,j++){
+                   if(arr1[i]>arr2[j])
+                       swap(arr1[i],arr2[j]);
+               }
+               //Second array swapping
+               if(j<m){
+                   for (j = 0; j + gap < m; j++)
+                   if (arr2[j] > arr2[j + gap])
+                       swap(arr2[j], arr2[j + gap]);
+               }
+           }
+        } 
 };
