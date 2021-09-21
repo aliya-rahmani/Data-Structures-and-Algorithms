ff839b64ef019b9dbc82a905cbaca44834e25741
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-09-21 22:16:15 +0530

    Create INTERVIEW.cpp

---
+++ implementation/INTERVIEW.cpp
@@ -0,0 +1,3 @@
+Name some applications which use Linked Lists.
+Both queues and stacks can be implemented using a linked list. Some of the other applications that use linked list are a binary tree, skip, unrolled linked list, hash table, etc.
+
