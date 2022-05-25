be5d49914be58da8ed3971aa6d756a6f64a2bd47
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2022-05-25 14:15:18 +0530

    Easy

---
+++ Revise/Pow(x, n).cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n==0) return 1;
+        else if (n == 1) return x;
+        double t = myPow(x,n/2);
+        if (n%2) {
+            return n<0 ? 1/x*t*t : x*t*t;
+        } else {
+            return t*t;
+        }
+    }
+}; 
