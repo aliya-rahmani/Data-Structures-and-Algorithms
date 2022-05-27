b16fbb6ea68cbe7105cc1ee990dd434c4f33f8dd
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2022-05-28 00:02:49 +0530

    Medium

---
+++ Revise/Recursion/Swap Nodes in Pairs.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+
+class Solution {
+public:
+ 
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* temp ;
+        temp = head->next;
+        head->next = swapPairs(head->next->next);
+        temp->next = head;
+        return temp;
+    }
+};
