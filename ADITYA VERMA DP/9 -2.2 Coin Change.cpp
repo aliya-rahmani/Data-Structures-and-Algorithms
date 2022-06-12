b3d2c26b2a3cf0387370fcca9038151aa1b95318
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2022-06-12 22:27:32 +0530

    Medium

---
+++ ADITYA VERMA DP/9 -2.2 Coin Change.cpp
@@ -0,0 +1,28 @@
+//numbers of coin needed for making it change
+
+class Solution {
+public:
+    int helper(vector<int>& arr, int n,int w){
+        int dp[n+1][w+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if(i==0) dp[i][j]=0;
+                if(j==0) dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i][j-arr[i-1]] + dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][w];
+        
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        
+        return helper(coins,coins.size(),amount);
+    }
+};
