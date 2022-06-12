f50b1bc320874037c91a46a96518856cfe179059
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2022-06-12 19:07:33 +0530

    Medium

---
+++ ADITYA VERMA DP/7. knapsack - Target Sum.cpp
@@ -0,0 +1,50 @@
+//https://leetcode.com/problems/target-sum/submissions/
+Same as count no. of subsets with given difference as we have to find subsets with + and - sign that will lead to difference eventually.
+
+class Solution {
+public:
+    int countSubsets(vector<int>& nums, int n, int M)
+    {
+        int t[n+1][M+1];
+
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=M; j++)
+            {
+                if(i==0)
+                    t[i][j]=0;
+                if(j==0)
+                    t[i][j]=1;
+            }
+        }
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=0; j<=M; j++)
+            {
+                if(nums[i-1]<=j)
+                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+
+        return t[n][M];  
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target)
+    {
+         target=abs(target);
+         int n=nums.size();
+         int sum=0;
+
+         for(int i=0; i<n; i++)
+             sum+=nums[i];
+
+        int M=(sum+target)/2;
+        if(sum<target||(sum+target)%2!=0)
+            return 0;
+        
+         return countSubsets(nums, n, M);
+    }  
+};
